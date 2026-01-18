5483951e13dee7c0e8ffecf1574f126881b1de36
SathwikReddyThaduru-226037595 <s226037595@deakin.edu.au>
2026-01-18 12:30:52 +1100

    updated

--- 5.3C/routes/books.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/books.controller');
-
-router.get('/', controller.getAllBooks);
-router.get('/:id', controller.getBookById);
-router.get('/integrity-check42', controller.integrityCheck);
-
-module.exports = router;
