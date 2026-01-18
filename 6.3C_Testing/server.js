544b71dfcabed0275334bd9c6afb2a228a450cf7
SathwikReddyThaduru-226037595 <s226037595@deakin.edu.au>
2026-01-18 20:41:28 +1100

    Removed 6.3C_Testing folder

--- 6.3C_Testing/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const mathRoutes = require('./routes/math.routes');
-
-app.use(express.json());
-app.use('/api/math', mathRoutes);
-
-module.exports = app;
